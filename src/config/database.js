a8ce0d62a5e2caaaac913c887fb4b039ebdd6873
LudovicCazenave <lcazenav@gmail.com>
2025-05-04 14:49:33 +0200

    fix bug

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,16 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(
   process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     define: {
       createdAt: "created_at",
       updatedAt: "updated_at",
     }
+  }
   }
 );
 
